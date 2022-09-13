18f76a907c1265d80c5923b89f7fb7f6305b5073
Elton Santos <elton.c.s1979@gmail.com>
2022-09-13 00:20:12 -0300

    Estudo JS

---
+++ JS/main.js
@@ -0,0 +1,69 @@
+function clicou(){
+    alert("Obrigado por clicar");
+};
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*///script_info = { name = , description = , author = , version = }
+/*
+var d = new Date();
+alert(d);
+alert(d.getDay);
+alert(d.getHours);
+alert(d.getMinutes);
+*/
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    count ++;
+}; 
+*/
+
+/*
+var idade = prompt("Qual a sua idade: ");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome:"banana", cor:"amarela"}, {nome:"uva", cor:"verde"}];
+console.log(frutas);
+*/
+
+/*
+var fruta = {nome:"banana", cor:"amarela"};
+console.log(fruta);
+*/
+
+/*
+var nome = "Elton Santos";
+var idade = 29;
+alert(nome + " tem " + idade + " anos.");
+
+var lista = ["macã", "pêra", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log (lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" | "));
+alert(lista[1]);
+*/
